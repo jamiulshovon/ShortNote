04db43a2577a6dd6407558150a0715cedc98bc53
Shovon <jamiulshovon@gmail.com>
2025-08-24 21:04:50 +0600

    Added delete state functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [notes, setNotes] = useState([
+    { id: 1, title: "Note 1" },
+    { id: 2, title: "Note 2" },
+  ]);
+
+  const setTitleHandler = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (title.trim() === "") {
+      return alert("Please provide a valid title");
+    }
+
+    const newNote = {
+      id: Date.now() + "",
+      title: title,
+    };
+
+    setNotes([newNote, ...notes]);
+    setTitle(""); 
+  };
+
+  const removeHandler = (noteId) => {
+    const updatedNotes = notes.filter((item) => item.id !== noteId);
+    setNotes(updatedNotes);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="addNote">Add Note: </label>
+        <input
+          type="text"
+          name="addNote"
+          id="addNote"
+          value={title}
+          onChange={setTitleHandler}
+        />
+        <button type="submit">Add Note</button>
+      </form>
+
+      
+      <div className="note-list">
+        <h2>All notes</h2>
+        <ul>
+          {notes.map((note) => (
+            <li key={note.id}>
+              <span>{note.title}</span>
+              <button>Edit</button>
+              <button onClick={() => removeHandler(note.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
